714238e595356be6ab642b8eb7156277c7eeb6c4
gusjak <70512255+gusjak@users.noreply.github.com>
2021-03-05 10:29:06 +0100

    Custom Fonts and Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 module.exports = {
-    purge: [
-        './src/**/*.html',
-    ],
-    theme: {},
-    variants: {},
-    plugins: [],
-}
+  purge: ['./src/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'light-gray': '#F0F0F0',
+        'primary-green': '#005B5D',
+        'accent-green': '#ABECB1',
+        'softer-black': '#111111',
+        'pastel-yellow': '#F0EEB6',
+        'pastel-red': '#E18377',
+        'pastel-blue-soft': '#BCD5EA',
+        'pastel-blue-clear': '#81C4E5',
+        'pastel-pink': '#F6D2DB',
+        'pastel-green-dark': '#7EAC8E',
+        'pastel-green-light': '#EEFBEF',
+      },
+      fontFamily: {
+        'text-title': 'Mikkelwind',
+        'text-regular': 'RalewayRegular',
+        'text-medium': 'RalewayMedium',
+        'text-bold': 'RalewayBold',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};

88d2c10be07421fe120d3ed46c2e451b00620ddc
gusjak <70512255+gusjak@users.noreply.github.com>
2021-03-12 11:05:30 +0100

    Query parameters
    Added function to get the query parameter values and add them to the sign-up form.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,3 +8,18 @@ let i = 0;
 setInterval(function () {
   document.title = i++ % 2 == 0 ? 'Welcome to' : "Mikkeller's Launch Event 🍻";
 }, 1500);
+
+// Get query parameter values
+window.onload = function () {
+  try {
+    const url_string = window.location.href.toLowerCase();
+    const url = new URL(url_string);
+    const name = url.searchParams.get('name');
+    const email = url.searchParams.get('email');
+
+    document.getElementById('name').value = name;
+    document.getElementById('email').value = email;
+  } catch {
+    console.log('You dun goofed');
+  }
+};

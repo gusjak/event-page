2ebabca7c31f21e7c38f49493015ace466c890ff
gusjak <70512255+gusjak@users.noreply.github.com>
2021-03-16 16:50:55 +0100

    Added animations to card arrows

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,30 +1,65 @@
 // Card click Events
 
 // Show/Hide Event Card Information
-document.querySelector('.show-event').onclick = () => {
-  document.querySelector('.event').classList.toggle('-translate-y-full');
+const eventButton = document.querySelector('.show-event');
+const eventButtonText = document.querySelector('.event-button-text');
+eventButton.onclick = () => {
+  eventButton.classList.toggle('-bottom-8');
+  eventButtonText.classList.toggle('-translate-y-12');
+  if (eventButtonText.textContent == 'Show Less') {
+    eventButtonText.textContent = 'Show More';
+  } else {
+    eventButtonText.textContent = 'Show Less';
+  }
+  document.querySelector('.event-card').classList.toggle('-translate-y-full');
   document.querySelector('.event-arrow').classList.toggle('rotate-180');
 };
 
 // Show/Hide Eat Card Information
-document.querySelector('.show-eat').onclick = () => {
+const eatButton = document.querySelector('.show-eat');
+const eatButtonText = document.querySelector('.eat-button-text');
+eatButton.onclick = () => {
+  eatButton.classList.toggle('-bottom-8');
+  eatButtonText.classList.toggle('-translate-y-12');
+  if (eatButtonText.textContent == 'Show Less') {
+    eatButtonText.textContent = 'Show More';
+  } else {
+    eatButtonText.textContent = 'Show Less';
+  }
   document.querySelector('.eat').classList.toggle('-translate-y-full');
   document.querySelector('.eat-arrow').classList.toggle('rotate-180');
 };
 
 // Show/Hide Music Card Information
-document.querySelector('.show-music').onclick = () => {
+const musicButton = document.querySelector('.show-music');
+const musicButtonText = document.querySelector('.music-button-text');
+musicButton.onclick = () => {
+  musicButton.classList.toggle('-bottom-8');
+  musicButtonText.classList.toggle('-translate-y-12');
+  if (musicButtonText.textContent == 'Show Less') {
+    musicButtonText.textContent = 'Show More';
+  } else {
+    musicButtonText.textContent = 'Show Less';
+  }
   document.querySelector('.music').classList.toggle('-translate-y-full');
   document.querySelector('.music-arrow').classList.toggle('rotate-180');
 };
 
 // Show Sign Up Comfirmation
-document.querySelector('.show-form').onclick = () => {
+const formButton = document.querySelector('.show-form');
+formButton.onclick = () => {
   document.querySelector('.form').classList.toggle('-translate-y-full');
   document.querySelector('.show-form').classList.toggle('hidden');
 };
 
-// Show/Hide Gift Card Information
-document.querySelector('.show-gift').onclick = () => {
+// Show/Hide Gift Card Information Front
+const giftButton = document.querySelector('.show-gift');
+giftButton.onclick = () => {
   document.querySelector('.gift').classList.toggle('-translate-y-full');
+};
+
+// Show/Hide Gift Card Information Back
+const giftBackButton = document.querySelector('.back');
+giftBackButton.onclick = () => {
+  document.querySelector('.gift').classList.remove('-translate-y-full');
 };

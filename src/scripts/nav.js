ba139d120ab06aeff7c3e4449ea0732ba427bca9
gusjak <70512255+gusjak@users.noreply.github.com>
2021-03-16 11:44:25 +0100

    Added visuals for active link in navbar

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -3,6 +3,41 @@ document.getElementById('nav-toggle').onclick = () => {
   document.getElementById('nav-content').classList.toggle('hidden');
 };
 
+// Toggle Henry's Hat
+
+const signupLink = document.querySelector('.signup');
+const eventLink = document.querySelector('.event');
+const aboutLink = document.querySelector('.about');
+const contactLink = document.querySelector('.contact');
+
+signupLink.onclick = () => {
+  document.querySelector('.hat1').classList.remove('hidden');
+  document.querySelector('.hat2').classList.add('hidden');
+  document.querySelector('.hat3').classList.add('hidden');
+  document.querySelector('.hat4').classList.add('hidden');
+};
+
+eventLink.onclick = () => {
+  document.querySelector('.hat2').classList.remove('hidden');
+  document.querySelector('.hat1').classList.add('hidden');
+  document.querySelector('.hat3').classList.add('hidden');
+  document.querySelector('.hat4').classList.add('hidden');
+};
+
+aboutLink.onclick = () => {
+  document.querySelector('.hat3').classList.toggle('hidden');
+  document.querySelector('.hat1').classList.add('hidden');
+  document.querySelector('.hat2').classList.add('hidden');
+  document.querySelector('.hat4').classList.add('hidden');
+};
+
+contactLink.onclick = () => {
+  document.querySelector('.hat4').classList.toggle('hidden');
+  document.querySelector('.hat1').classList.add('hidden');
+  document.querySelector('.hat2').classList.add('hidden');
+  document.querySelector('.hat3').classList.add('hidden');
+};
+
 // Shrink Navbar when scrolling
 window.onscroll = () => {
   scrollFunction();
